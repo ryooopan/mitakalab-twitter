41bac1b0cf51592302e50cc05b4ac653fbd97e55
Ryo Suzuki <ryooopan@googlemail.com>
2013-07-02 22:31:17 +0900

    create sign up view and add has_secure_password

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 MitakalabTwitter::Application.routes.draw do
-  get "users/index"
-  get "users/show"
-  get "users/new"
-  get "users/edit"
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
