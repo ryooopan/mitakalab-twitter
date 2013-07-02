c2deaaa5304f87c1c6758cc326503f70324a730d
Ryo Suzuki <ryooopan@googlemail.com>
2013-07-02 21:20:27 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MitakalabTwitter::Application.config.secret_key_base = 'acea85a98cd6e46f29403d5c5014a53cac97110b838affa2629b92925a934b6dc0ed76416bc91826bb69cf07bb18eaa0f41741dcbe92a95f0ab08e4ded8ab90b'
