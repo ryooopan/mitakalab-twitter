a763133617717c7af404279055568ddd7dd3dfef
Ryo Suzuki <ryooopan@googlemail.com>
2013-07-03 16:26:47 +0900

    add features of tweet index, show, new, and edit

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,37 @@
 class TweetsController < ApplicationController
-  before_action :authorize, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authorize, :only => [:new, :create, :edit, :update, :destroy]
 
   def index
+    @tweets = Tweet.all
   end
 
   def show
+    @tweet = Tweet.find(params[:id])
   end
 
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+    @tweet.save
+    redirect_to @tweet
   end
 
   def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(tweet_params)
+    redirect_to @tweet
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:content)
   end
 end
--- db/migrate/20130703014330_create_tweets.rb
+++ db/migrate/20130703014330_create_tweets.rb
@@ -1,7 +1,7 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
-      t.references :user, index: true
+      t.references :user, :index => true
       t.text :content
 
       t.timestamps
